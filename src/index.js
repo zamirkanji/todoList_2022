cab51d27baa9cfb76ed7c8b414a249c04e03e49c
Zamir Kanji <zamirkanji@comcast.net>
2022-01-18 20:22:02 -0800

    changing layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import menuIcon from './img/menu.png';
+
+const menuEventListener = (() => {
+    const menuLogo = document.querySelector('.menu-icon');
+    const mainSidebar = document.querySelector('.main-sidebar');
+    menuLogo.addEventListener('click', () => {
+        console.log('test');
+        mainSidebar.classList.toggle('display');
+    })
+})();
